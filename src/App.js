0bb9ecb2da20219a4fc752177159d5e626d2df02
pamdo-iscte <pamdo@iscte-iul.pt>
2022-10-21 18:06:52 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <p className="text-red-500 font-bold text-9xl">maior da aldeia</p>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
